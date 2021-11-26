4c05e388bc11dfd817fe936e1cd0f5c621837b81
Martin <martinpfatrisch@gmail.com>
2021-11-26 11:19:04 +0100

    Created the store and wired up getters for the CoachesList component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,14 +7,26 @@
         <router-link to="/register">Register as Coach</router-link>
       </button>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found...</h3>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
 
 <style lang="css" scoped></style>
