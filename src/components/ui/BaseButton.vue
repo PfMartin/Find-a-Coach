1dc4377e4c753122a15a0e9a439f0769d010c4bd
Martin <martinpfatrisch@gmail.com>
2021-11-29 08:25:41 +0100

    Fixed bug in the Refresh button

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -2,7 +2,7 @@
   <button v-if="!link" :class="mode">
     <slot></slot>
   </button>
-  <router-link v-if="link" :to="to" :class="mode">
+  <router-link v-else :to="to" :class="mode">
     <slot></slot>
   </router-link>
 </template>
